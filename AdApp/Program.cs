9a6df9c9cc756356770755e434f21e4e62348dc9
Nadeem <nadeemlari@gmail.com>
2022-01-01 18:06:30 +0530

    Initial commit

---
+++ AdApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+
+//DownloadBlobWithConnectionString();
+//DownloadBlobWithAdApp();
+DownloadBlobWithManagedIdentity();
+
+
+
+void DownloadBlobWithConnectionString()
+{
+    const string blobConnectionString = "azure storage connection string here";
+    const string containerName = "data";
+    const string localBlob = "c:\\mnl\\MSR2.html";
+    const string blobName = "MSR2.html";
+
+    var blobServiceClient = new BlobServiceClient(blobConnectionString);
+    var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+    var blobClient = containerClient.GetBlobClient(blobName);
+    blobClient.DownloadTo(localBlob);
+    Console.WriteLine("Blob Downloaded");
+
+}
+void DownloadBlobWithAdApp()
+{
+    const string tenantId = "tenant id here";
+    const string clientId = "client id here";
+    const string clientSecret = "secret here";
+    const string localBlob = "c:\\mnl\\MSR2.html";
+
+    var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    var blobUri = new Uri("https://mnl2000.blob.core.windows.net/data/MSR2.html");
+    var blobClient = new BlobClient(blobUri, clientSecretCredential);
+    blobClient.DownloadTo(localBlob);
+    Console.WriteLine("Blob Downloaded");
+
+}
+void DownloadBlobWithManagedIdentity()
+{
+    const string localBlob = "c:\\mnl\\MSR2.html";
+    var credential = new ChainedTokenCredential(new AzureCliCredential(),new ManagedIdentityCredential());
+    var blobUri = new Uri("https://mnl2000.blob.core.windows.net/data/MSR2.html");
+    var blobClient = new BlobClient(blobUri, credential);
+    blobClient.DownloadTo(localBlob);
+    Console.WriteLine("Blob Downloaded");
+
+}
+
+
---
+++ KeyValutApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using Azure.Security.KeyVault.Secrets;
+
+// ReadKeyVault();
+KeyVaultEncryption();
+
+void ReadKeyVault()
+{
+    const string tenantId = "ac11fdf6-cc10-4290-9f63-eaabb6cd0b33";
+    const string clientId = "a1dd25e0-c3bd-444f-9289-564719ec1b94";
+    const string clientSecret = "Wi57Q~X4Wge3bXDTfyvShYajyy3y5X.~jIjWs";
+    const string secretName = "dbPassword";
+
+    var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    var keyVaulturl = new Uri("https://mnl2002.vault.azure.net/");
+    var secretClient = new SecretClient(keyVaulturl, clientSecretCredential);
+    var secret = secretClient.GetSecret(secretName);
+    Console.WriteLine($"Secret value id is {secret.Value.Value}");
+
+}
+
+void KeyVaultEncryption()
+{
+    const string tenantId = "tenant id here";
+    const string clientId = "client id here";
+    const string clientSecret = "secret here";
+    const string keyName = "key1";
+    const string textToEncrypt = "this ia plain text";
+
+    var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    var keyVaulturl = new Uri("https://mnl2002.vault.azure.net/");
+    var secretClient = new KeyClient(keyVaulturl, clientSecretCredential);
+    var key = secretClient.GetKey(keyName);
+    var cryptoClient = new CryptographyClient(key.Value.Id, clientSecretCredential);
+    var textToBytes = Encoding.UTF8.GetBytes(textToEncrypt);
+    var encryptedResult = cryptoClient.Encrypt(EncryptionAlgorithm.RsaOaep256, textToBytes);
+    Console.WriteLine("Encrypted text");
+    Console.WriteLine(Convert.ToBase64String(encryptedResult.Ciphertext));
+    var decryptResult = cryptoClient.Decrypt(EncryptionAlgorithm.RsaOaep256, encryptedResult.Ciphertext);
+
+    Console.WriteLine("Plain text");
+    Console.WriteLine(Encoding.UTF8.GetString(decryptResult.Plaintext));
+
+
+
+
+}
